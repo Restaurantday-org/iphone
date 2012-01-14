bbaf2be3f3e2b6b5b785fbae7e6494a6b488a7f9
Janne Käki <janne.kaki@futurice.com>
2012-01-14 17:34:46 +0200

    restaurants starting to be represented awesomely

---
+++ Restaurant Day/Views/RestaurantCell.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantCell.h
+//  Restaurant Day
+//
+//  Created by Janne Käki on 1/14/12.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestaurantCell : UITableViewCell
+
+@end
---
+++ Restaurant Day/Views/RestaurantCell.m
@@ -0,0 +1,29 @@
+//
+//  RestaurantCell.m
+//  Restaurant Day
+//
+//  Created by Janne Käki on 1/14/12.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#import "RestaurantCell.h"
+
+@implementation RestaurantCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
